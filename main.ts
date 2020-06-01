35adef9bcf185ad03ccff4c99d3c663188d364ab
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-06-01 16:17:10 +0300

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
-})
+namespace progressAnimation{
+
+
+export function progress_bar (ledsayisi: number, sira: number, durum: boolean):void {
+    if (durum) {
+        for (let satir = 0; satir <= 4; satir++) {
+            for (let sutun = 0; sutun <= ledsayisi - 1; sutun++) {
+                led.plot(satir, sutun + sira)
+                for (let index = 0; index <= 50; index++) {
+                    led.setBrightness(index * 4 + 55)
+                    basic.pause(10)
+                }
+                led.setBrightness(55)
+            }
+        }
+        for (let index = 0; index < 4; index++) {
+            led.enable(false)
+            basic.pause(100)
+            led.enable(true)
+            basic.pause(100)
+        }
+        led.setBrightness(255)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+}
+
+
+
+
+
+}
